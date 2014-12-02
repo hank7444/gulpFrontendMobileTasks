a74c84ebc587d437c8225d98cf1b15cf48d39824
Hank Kuo <hank_kuo@hiiir.com>
2014-12-02 17:06:19 +0800

    1. 增加了對html頁面與js自動產生code的機制 2. index.html增加了對手機icon的說明 3. 增加了humans.txt與robots.txt的說明

--- gulpfile.js
+++ gulpfile.js
@@ -28,10 +28,12 @@ var runSequence = require('run-sequence');
 // inject
 var inject = require("gulp-inject");
 var htmlbuild = require('gulp-htmlbuild'); // 字串寫入
-
 var es = require('event-stream');
 
-var basePath = path.join(__dirname) + '/';
+// test
+var fs = require('fs');
+var tap = require('gulp-tap'); // 取得檔案名稱
+var rename = require("gulp-rename");
 
 // 用絕對路徑會導致gulp-watch對新增檔案的監聽會失效..@@
 var filefolder = {
@@ -46,10 +48,21 @@ var filefolder = {
     'html': 'html/**/*.html',
     'htmldist': 'dist/html/**/*.html',
     'css': {
+        'all': 'css/**/*.css',
         'global': ['css/global/normalize.css', 'css/global/**/!(normalize)*.css'],
         'normal': ['css/**/*.css', '!css/global/**/*.css']
     },
-    'sass': 'sass/**/*.{sass, scss}'
+    'sass': 'sass/**/*.{sass, scss}',
+    'test': {
+        'html': {
+            'html': 'test/html/*.html',
+            'script': 'test/html/script/*.js'
+        },
+        'js': {
+            'html': 'test/js/*.html',
+            'script': 'test/js/script/**/*.js'
+        }
+    }
 };
 
 
@@ -69,21 +82,21 @@ var watchStatus = {
 };
 
 
+
 gulp.task('browser-sync', function() {
 
-    gulp.src([
+    var syncAry = [
         filefolder.html,
-        filefolder.css,
+        filefolder.css.all,
         filefolder.js.all,
-        filefolder.img
-    ])
+        filefolder.img,
+        filefolder.test.html.html,
+        filefolder.test.html.script
+    ];
+
+    gulp.src(syncAry)
         .pipe(plumber())
-        .pipe(watch([
-                filefolder.html,
-                filefolder.css,
-                filefolder.js,
-                filefolder.img
-        ])).pipe(reload({
+        .pipe(watch(syncAry)).pipe(reload({
             stream: true
         }));
 
@@ -261,19 +274,223 @@ gulp.task('clean-img', function() {
 gulp.task('clean-all', ['clean', 'clean-img']);
 
 
+// ********** 測試用 ***********
+// 測試用libs
+var getTestLibAry = {
+    'js': [
+        '../../js/test/jquery-1.10.2.min.js',
+        '../../js/test/mocha.js',
+        '../../js/test/chai.js',
+        '../../js/test/chai-jquery.js',
+    ],
+    'css': [
+        '../../js/test/mocha.css'
+    ]
+};
+
+
+// 要監聽產生的資料夾位置
+var jsTestFolderAry = [
+    filefolder.js.script,
+    filefolder.js.lib
+];
+
+
+// 測試用function
+var getPathName = function(filepath) {
+    var name = path.basename(filepath);
+    return name.substr(0, name.lastIndexOf('.'));
+};
+
+var getJsFolderName = function(filepath) {
+
+    var dirname = __dirname + '/js/';
+    var folderName = filepath.replace(dirname, '');
+
+    return folderName.substr(0, folderName.lastIndexOf('/'));
+};
+
+
+// 建立測試用的code
+var destTestJs = function(name, foldername, testType) {
+
+    var dir = './' + testType + '/script/';
+    var filepath = '';
+
+    if (foldername) {
+        dir = './' + testType + '/' + foldername + '/script/';
+    }
+    filepath = dir + name + '.js';
+
+    fs.exists(filepath, function(exists) {
+
+        if (exists) {
+            return false;
+        }
+        gulp.src('./test/default.js')
+            .pipe(rename(function(path) {
+                path.dirname = dir;
+                path.basename = name;
+                path.extname = ".js";
+
+            }))
+            .pipe(gulp.dest("./test"))
+            .pipe(reload({
+                stream: true
+            }));
+    });
+};
+
+
+
+var createTestHtmlHash = {
+    'html': function(name, filepath, foldername, testType) {
+
+        gulp.src(filepath)
+            .pipe(htmlbuild({
+                // add a header with this target
+                htmltest: function(block) {
+                    block.end('<div id="mocha"></div>');
+                },
+                csstest: function(block) {
+                    var cssLibAry = getTestLibAry.css;
+
+                    es.readArray(cssLibAry.map(function(str) {
+                        return '<link rel="stylesheet" href="' + str + '">';
+                    })).pipe(block);
+                },
+                jstest: function(block) {
+
+                    var jsLibAry = getTestLibAry.js.slice(0);
+
+                    if (name) {
+                        jsLibAry.push('./script/' + name + '.js');
+                    }
+
+                    es.readArray(jsLibAry.map(function(str) {
+                        return '<script src="' + str + '"></script>';
+                    })).pipe(block);
+                },
+            }))
+            .pipe(gulp.dest('./test/' + testType))
+            .pipe(reload({
+                stream: true
+            }));
+    },
+    'js': function(name, filepath, foldername, testType) {
+
+        // 產生測試用的html
+        var htmlPath = 'test/' + testType + '/' + foldername + '/' + name + '.html';
+        gulp.src('./test/default.html')
+            .pipe(rename(function(path) {
+                path.dirname = testType + '/' + foldername;
+                path.basename = name;
+                path.extname = ".html";
+            }))
+            .pipe(htmlbuild({
+                js: function(block) {
+
+                    var jsLibAry = getTestLibAry.js.slice(0);
+
+                    jsLibAry = jsLibAry.map(function(value) {
+                        return '../' + value;
+                    });
+
+                    if (name) {
+                        jsLibAry.push('../../../' + testType + '/' + foldername + '/' + name + '.js');
+                        jsLibAry.push('./script/' + name + '.js');
+                    }
+                    es.readArray(jsLibAry.map(function(str) {
+                        return '<script src="' + str + '"></script>';
+                    })).pipe(block);
+                },
+            }))
+            .pipe(gulp.dest("./test"))
+            .pipe(reload({
+                stream: true
+            }));
+     
+    }
+};
+
+var destTestHtml = function(name, filepath, foldername, testType) {
+    createTestHtmlHash[testType](name, filepath, foldername, testType);
+};
+
+
+gulp.task('test-html', function() {
+
+    var testType = 'html';
+
+    // 監聽html檔案, 並產生對應的測試用html與js,
+    // html會自動加上測試用的lib與對應相同名稱的js,
+    // js部分如果已經存在則不覆蓋(避免寫的測試碼不見)
+    gulp.src(filefolder.html)
+        .pipe(watch(filefolder.html, function(files) {
+            files.pipe(tap(function(file, t) {
+                var name = getPathName(file.path);
+                destTestJs(name, null, testType);
+                destTestHtml(name, file.path, null, testType);
+            }));
+        }))
+        .pipe(plumber())
+        .pipe(filter(watchStatus.isNotDeleted))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task('test-js', function() {
+
+    var testType = 'js';
+
+    // 監聽js/script檔案, 產生對應的測試用html
+    // html會自動加上測試用的lib與測試用的js
+    // html如果已存在則不覆蓋(避免使用者新增的部分被覆蓋掉)
+    gulp.src(jsTestFolderAry)
+        .pipe(watch(jsTestFolderAry, function(files) {
+
+            files.pipe(tap(function(file, t) {
+              
+                var name = getPathName(file.path);
+                var folder = getJsFolderName(file.path);
+                destTestJs(name, folder, testType);
+                destTestHtml(name, file.path, folder, testType);
+            }));
+        }))
+        .pipe(filter(watchStatus.isNotDeleted))
+        .pipe(reload({
+            stream: true
+        }));
+});
 
 
 
 
 
-gulp.task('livereload', ['browser-sync', 'server']);
+gulp.task('test-html-watch', function() {
+    gulp.watch(filefolder.html, ['test-html']);
+});
 
+gulp.task('test-js-watch', function() {
+     gulp.watch(jsTestFolderAry, ['test-js']);
+});
+
+
+
+
+// 開發用
 gulp.task('server', ['sass', 'development']);
 
+// 開發用-及時預覽
+gulp.task('live', ['browser-sync', 'server']);
+
+// 發佈用
 gulp.task('dist', function(cb) {
     runSequence('clean', ['concat-css', 'minify-css', 'minify-js', 'move-js', 'production']);
 });
 
+// 發佈用-壓縮圖片
 gulp.task('dist-img', function(cb) {
     runSequence('clean-all', ['dist', 'minify-img']);
 });
